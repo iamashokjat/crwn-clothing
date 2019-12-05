62e8af7ce0c82eb9f36ffa435b029b9389f3b67a
iamashokjat <ashokjakad@gmail.com>
2019-12-05 13:37:18 +0530

    Converted array to object

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,20 @@
- import React from 'react'; 
-import './collection-preview.styles.scss';  
+import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-const collectionPreview = ({title,items}) =>(
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1> 
-        <div className="preview">
-            {items
-            .filter((item,index) => index<4)
-            .map(({id,...itemProps}) =>( 
-                <CollectionItem key={id} {...itemProps} />
-            ))}
-        </div>
-    </div>
-) 
+import './collection-preview.styles.scss';
 
-export default collectionPreview;
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
